bc8ad73d19b0fb59c055ebf28ef02d879144f4bb
elderlytechstudent <nicholascaucci1997@gmail.com>
2026-02-09 21:18:43 -0800

    Create 2-7-26.js

---
+++ 2-7-26.js
@@ -0,0 +1,10 @@
+function getLandingStance(startStance, rotation) {
+
+  const flips = Math.floor(Math.abs(rotation) / 180);
+
+  if (flips % 2 !== 0) {
+    return startStance === "Regular" ? "Goofy" : "Regular";
+  }
+
+  return startStance;
+}
